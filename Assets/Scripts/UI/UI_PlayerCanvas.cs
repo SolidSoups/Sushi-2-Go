e93f656e23e078dbc5b1300c4ae49296e8d3a4ef
SolidSoups <156454005+SolidSoups@users.noreply.github.com>
2024-11-19 17:50:53 +0100

    Organized Game hierchy and directory
    Organized the entire directory of the game, adding color coding and good naming.
    Prefabbed certain gameobjects in the main scene to allow smoother editing when working together.
    Added a color coding asset to organize the heirchy of the game

---
+++ Assets/Scripts/UI/UI_PlayerCanvas.cs
@@ -0,0 +1,41 @@
+using System.Data.Common;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class UI_PlayerCanvas : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI _finalScoreText;
+    [SerializeField] private TextMeshProUGUI _highScoreText;
+    [SerializeField] private TextMeshProUGUI _countDownText;
+    [SerializeField] private GameObject _countDownObj;
+    [SerializeField] private TimerScore _timerScore;
+
+    public void SetHighScore(int score) => _scoreText.text = "HIGHSCORE: " + score.ToString();
+
+    public void HighScoreUpdate()
+    {
+        //Checks if there are a highscore
+        if (PlayerPrefs.HasKey("SavedHighScore"))
+        {
+            //If the new score is higher then the saved one
+            if(_timerScore.Score > PlayerPrefs.GetInt("SavedHighScore"))
+            {
+                //Sets the new highscore
+                PlayerPrefs.SetInt("SavedHighScore", _timerScore.Score);
+            }   
+        }
+        else
+        {
+            //if there is no highscore. set it
+            PlayerPrefs.SetInt("SavedHighScore", _timerScore.Score);
+        }
+        
+        PlayerPrefs.Save();
+        //updating the TMP
+        _finalScoreText.text = _timerScore.Score.ToString();
+        _highScoreText.text =  PlayerPrefs.GetInt("SavedHighScore").ToString();
+    }
+}
